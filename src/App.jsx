e4173649d68fc24f1f920ff685bf24ff2d6051bf
harsh1924 <harshrpanwar@gmail.com>
2025-04-27 12:52:58 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TrafficLight from "./ui/TrafficLight"
+
+function App() {
+
+  return (
+    <>
+      <TrafficLight />
+    </>
+  )
+}
+
+export default App
---
+++ src/ui/TrafficLight.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrafficLight = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TrafficLight
